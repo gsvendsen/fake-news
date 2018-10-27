23123db7dddbfbc0c099dd8a65de81a206aee12c
Gustav Svendsen <gsvendsen99@gmail.com>
2018-10-27 19:15:36 +0200

    Added bootstrap styling on elements and removed old stylesheet

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 require_once __DIR__.'/data.php';
 require_once __DIR__.'/functions.php';
 
-usort($newsItems, "sortDates");
+// Sorts the array $newsItems using callback function comparedates()
+uSort($newsItems, 'compareDates')
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,24 +13,38 @@ usort($newsItems, "sortDates");
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Fake News Feed</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     </head>
     <body>
-      <div class="wrapper">
-        <?php foreach ($newsItems as $newsItem): ?>
-        <article>
-          <h2><?= $newsItem['title'] ?></h2>
-          <h3><?= getUserName($newsItem['author'], $authors).' ('.$newsItem['author'].')'; ?></h3>
-          <p>Published: <?= $newsItem['publishedDate'] ?></p>
-          <p><?= $newsItem['content'] ?></p>
-          <div class="likeBox">
-            <img class="thumbsUp" src="https://png.icons8.com/metro/1600/thumb-up.png" alt="">
-            <div class="likeCounter">
-              <p><?= $newsItem['likeCount'] ?></p>
-            </div>
-          </div>
-        </article>
-        <?php endforeach; ?>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Fake News</a>
+        </nav>
+        <?php
+        // Loops over each item in data array newsItems and creates article with the current item's information
+        foreach ($newsItems as $newsItem): ?>
+        <div class="row">
+            <div class="col-sm-6 mx-auto m-3">
+                <div class="card">
+                    <div class="card-header">
+                        <h4><?= $newsItem['title'] ?></h4>
+                    </div>
+                    <div class="card-body">
+                        <p><?= $newsItem['content'] ?></p>
+                        <p><?= getUserName($newsItem['author'], $authors); ?></p>
+                        <p class="font-weight-light"><?= $newsItem['publishedDate'] ?></p>
+                        <p>
+                        </div>
+                        <div class="card-footer bg-transparent">
+                            <div class="row">
+                                <p class="m-2">Likes: <?= $newsItem['likeCount'] ?></p>
+                                <button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">Like</button>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+
       </div>
     </body>
 </html>
