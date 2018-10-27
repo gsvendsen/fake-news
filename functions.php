8b2684faec356ee08247d953d7b72846d494adc8
Gustav Svendsen <gsvendsen99@gmail.com>
2018-10-27 19:18:08 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,25 +3,25 @@
 declare(strict_types=1);
 
 /**
- * [getUserName Fetches a user's full name using a unique userId]
- * @param  string $userId   [The userId which is searched for]
- * @param  array  $userList [List of all users and their information]
- * @return string           [Returns the user's full name which has the userId]
+ * getUserName Fetches a user's full name using a unique userId
+ * @param  string $userId   The userId which is searched for
+ * @param  array  $userList List of all users and their information
+ * @return string           Returns the user's full name which has the userId
  */
 function getUserName(string $userId, array $userList): string{
-  foreach($userList as $user){
-    if ($user['userId'] == $userId) {
-        return $user['name'];
+    foreach($userList as $user){
+        if ($user['userId'] == $userId) {
+            return $user['name'];
+        }
     }
-  }
 }
 
 /**
- * [sortDates Function that is used for usort, sorts two dates at a time]
- * @param  [type] $a [The first date]
- * @param  [type] $b [The second date]
- * @return [type]    [Returns the correct order]
+ * sortDates Callback function that is used for uSort, sorts two dates at a time
+ * @param  array $a The first newsItem
+ * @param  array $b The second newsItem
+ * @return int    Returns the correct sort
  */
-function sortDates($a, $b) {
+function compareDates(array $a,array $b):int{
     return strtotime($b['publishedDate']) - strtotime($a['publishedDate']);
 }
