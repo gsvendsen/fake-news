79928eb82d08c69e186f07439063eabf52943fc6
Gustav Svendsen <gsvendsen99@gmail.com>
2018-10-31 17:14:14 +0100

    Added eventlistener for top button being clicked

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+// Selects the top button
+const topButton = document.querySelector(".top-button");
 
+// Scrolls window to top if top button is clicked
+topButton.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+})
