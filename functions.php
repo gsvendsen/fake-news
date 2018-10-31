a48b6a2b64efb01bf899b8aac7db9c391fd959c4
Gustav Svendsen <gsvendsen99@gmail.com>
2018-10-31 19:19:07 +0100

    Improved documentation

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 /**
  * getUser Fetches a user's data array using a unique userId
- * @param  int $userId   The userId which is searched for
+ * @param  int $userId The userId which is searched for
  * @param  array $userList List of all users and their information
  * @return array
  */
@@ -16,9 +16,9 @@ function getUser(int $userId, array $userList): array{
 }
 
 /**
- * compareDates Callback function that is used for uSort, sorts two dates at a time
- * @param  array $a The first newsItem
- * @param  array $b The second newsItem
+ * compareDates Function that compares the publishedDate of two arrays
+ * @param  array $a First array
+ * @param  array $b Second array
  * @return int
  */
 function compareDates(array $a,array $b):int{
