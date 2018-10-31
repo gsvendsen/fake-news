1c40a5242c9ef982aa7c1764d7e021d8e7f78fcd
Gustav Svendsen <gsvendsen99@gmail.com>
2018-10-31 18:27:44 +0100

    Added missing documentation

--- index.php
+++ index.php
@@ -93,7 +93,7 @@ if(isset($_GET['filterAuthor'])){
     <?php endforeach; ?>
     <div class="top-button">
         <p class="up-arrow">⇧</p>
-    </div>
+    </div><!-- /top-button -->
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
